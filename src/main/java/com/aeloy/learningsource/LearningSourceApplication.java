08725afdd7baba9a265e88b1ab0cf3056e748131
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2019-12-02 20:59:15 +1100

    initial commit

---
+++ src/main/java/com/aeloy/learningsource/LearningSourceApplication.java
@@ -0,0 +1,13 @@
+package com.aeloy.learningsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeloy/learningsource/LearningSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeloy.learningsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
